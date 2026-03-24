322e28f3c342c6004eaa4b793d3df460a909358b
Padmashree <shreerishi2709@gmail.com>
2026-03-24 11:53:37 +0530

    removed small issues

--- pages/home_page.py
+++ pages/home_page.py
@@ -6,13 +6,13 @@ from pages.base_page import BasePage
 
 class HomePage(BasePage):
 
-    # Locators
+  
     NAV_SHOP = (By.CSS_SELECTOR, "li.shop-hover")
     PROFILE_ICON = (By.CSS_SELECTOR, "a[href='/accounts/login/']")
     ACCOUNT_TOGGLE = (By.CSS_SELECTOR, "div.account-toggle")
     LOGOUT_LINK = (By.CSS_SELECTOR, "a[href='/accounts/logout/']")
 
-    # Category links
+   
     CATEGORY_URLS = {
         "Lipsticks": "/shop/category/lipsticks/",
         "Skincare": "/shop/category/skincare/",
@@ -24,9 +24,8 @@ class HomePage(BasePage):
         super().__init__(driver)
 
     def wait_for_homepage(self):
-        # Render free hosting — server sleep state la irukum
-        # 3 mins (180 secs) explicit wait — server wake up aaagum varaikkum
-        print("⏳ Waiting for Render server to wake up (max 3 mins)...")
+       
+        print(" Waiting for Render server to wake up (max 3 mins)...")
         render_wait = WebDriverWait(self.driver, 180)
         render_wait.until(EC.presence_of_element_located(self.NAV_SHOP))
         self.wait_seconds(2)
@@ -38,7 +37,7 @@ class HomePage(BasePage):
         print(" Clicked Profile Icon")
 
     def hover_shop_and_click_category(self, category_name):
-        # Hover over Shop
+       
         from selenium.webdriver.common.action_chains import ActionChains
         shop_element = self.wait.until(
             EC.presence_of_element_located(self.NAV_SHOP)
@@ -47,7 +46,7 @@ class HomePage(BasePage):
         self.wait_seconds(1)
         print(f" Hovered over Shop menu")
 
-        # Click category link
+      
         category_url = self.CATEGORY_URLS[category_name]
         category_locator = (By.CSS_SELECTOR, f"a[href='{category_url}']")
         self.click(*category_locator)
